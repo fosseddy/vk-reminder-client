d9f8b9e4984ad97d6d08434d3212f3fcda7dbab3
Mark Fosseddy <markfosseddy@gmail.com>
2022-03-14 15:30:01 +0500

    rename pages to views

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from '../pages/home.vue';
+import HomeView from '../views/home.vue';
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomePage
-    }
+    { path: '/', name: 'home', component: HomeView }
   ]
 });
