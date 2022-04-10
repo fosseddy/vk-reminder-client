c6b8da0926e8188ac67cc44693ae2d7912e7712b
Mark Fosseddy <markfosseddy@gmail.com>
2022-04-10 09:12:12 +0500

    wipe

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { fileURLToPath, URL } from 'url';
+import { fileURLToPath, URL } from 'url'
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -10,4 +11,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-});
+})
