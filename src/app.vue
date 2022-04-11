15179d7596ce458f7d988d7285059f5705cfe0f1
Mark Fosseddy <markfosseddy@gmail.com>
2022-04-11 20:40:54 +0500

    add auto login

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,42 @@
+<script>
+import * as vk from "@/vk";
+
+export default {
+	data() {
+		return {
+			loading: false
+		};
+	},
+
+	async created() {
+		this.loading = true;
+		const res = await vk.getLoginStatus().catch(err => {
+			console.log("WTF LOL");
+			console.log(err);
+		});
+
+		if (res.session) {
+			const id = localStorage.getItem("userid");
+			const { response } = await vk.getUserInfo(id);
+			const user = response[0];
+
+			if (user) {
+				this.$store.commit("auth/setUser", {
+					id: user.id,
+					firstname: user.first_name,
+					lastname: user.last_name,
+					avatar: user.photo_100
+				});
+				this.$router.push("/dashboard");
+			}
+		}
+
+		this.loading = false;
+	}
+};
+</script>
+
 <template>
-<router-view></router-view>
+<h1 v-if="loading">Loading...</h1>
+<router-view v-else></router-view>
 </template>
