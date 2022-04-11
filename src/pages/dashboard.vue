33caf951cd7b1e19d6e59123a1277b5d175a847b
Mark Fosseddy <markfosseddy@gmail.com>
2022-04-11 20:08:59 +0500

    add dashboard page

---
+++ src/pages/dashboard.vue
@@ -0,0 +1,38 @@
+<script>
+async function vkLogout() {
+	return new Promise(resolve => VK.Auth.logout(res => resolve(res)));
+}
+
+export default {
+	data() {
+		return {};
+	},
+
+	computed: {
+		user() {
+			return this.$store.state.auth.user;
+		}
+	},
+
+	methods: {
+		async logout() {
+			await vkLogout().catch(console.error);
+			this.$router.push("/");
+		}
+	},
+};
+</script>
+
+<template>
+<div>
+	<h1>This is Dashboard Page</h1>
+
+	<img :src="user.avatar" />
+	<p>{{ user.firstname }} {{ user.lastname }}</p>
+
+	<button @click="logout">Logout</button>
+</div>
+</template>
+
+<style scoped>
+</style>
