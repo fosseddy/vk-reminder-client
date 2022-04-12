183d075b3ab14cd277246b0a4d3e8250f793fc88
Mark Fosseddy <markfosseddy@gmail.com>
2022-04-12 10:24:20 +0500

    add strict

--- src/store.js
+++ src/store.js
@@ -2,7 +2,8 @@ import { createStore } from "vuex";
 import { auth } from "@/modules/auth/store.js";
 
 const store = createStore({
-	modules: { auth }
+	modules: { auth },
+	strict: process.env.NODE_ENV !== "production"
 });
 
 export { store };
