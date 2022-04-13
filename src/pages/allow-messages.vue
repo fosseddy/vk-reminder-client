7d33fd1f1c348b6e4f2572ca7f9b1dd592b957a1
Mark Fosseddy <markfosseddy@gmail.com>
2022-04-13 12:12:52 +0500

    add allow messages widget

--- src/pages/allow-messages.vue
+++ src/pages/allow-messages.vue
@@ -9,6 +9,33 @@ export default {
 	},
 
 	methods: {
+	},
+	
+	created() {
+		VK.Observer.subscribe(
+			"widgets.allowMessagesFromCommunity.allowed",
+			id => {
+				console.log(id, "allowed");
+			}
+		);
+
+		VK.Observer.subscribe(
+			"widgets.allowMessagesFromCommunity.denied",
+			id => {
+				console.log(id, "denied");
+			}
+		);
+
+		VK.Widgets.AllowMessagesFromCommunity(
+			"vk_send_message",
+			{ height: 30 },
+			202435034
+		);
+	},
+
+	unmounted() {
+		VK.Observer.unsubscribe("widgets.allowMessagesFromCommunity.allowed");
+		VK.Observer.unsubscribe("widgets.allowMessagesFromCommunity.denied");
 	}
 };
 </script>
@@ -16,5 +43,6 @@ export default {
 <template>
 <div>
 	<h1>This is Allow Messages Page</h1>
+	<div id="vk_send_message"></div>
 </div>
 </template>
