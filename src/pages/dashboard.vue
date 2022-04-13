dee01a87686685ca99c3c41ef69ae55445f33933
Mark Fosseddy <markfosseddy@gmail.com>
2022-04-13 20:23:14 +0500

    add logout mutation, debug for messages

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -7,13 +7,32 @@ export default {
   },
 
   methods: {
-    // @TODO(art): use logout mutation
     async logout() {
       await VK.Auth.logoutAsync();
       localStorage.removeItem("userid");
-      this.$store.commit("auth/setUser", null);
+      this.$store.commit("auth/logout");
       this.$router.push("/");
     }
+  },
+
+  created() {
+    VK.Observer.subscribe(
+      "widgets.allowMessagesFromCommunity.denied",
+      async () => await this.logout()
+    );
+  },
+
+  mounted() {
+    VK.Widgets.AllowMessagesFromCommunity(
+      "vk_send_message",
+      { height: 30 },
+      // @TODO(art): get community id from server?
+      202435034
+    );
+  },
+
+  unmounted() {
+    VK.Observer.unsubscribe("widgets.allowMessagesFromCommunity.denied");
   }
 };
 </script>
@@ -26,6 +45,8 @@ export default {
   <p>{{ user.firstname }} {{ user.lastname }}</p>
 
   <button @click="logout">Logout</button>
+  <br /><br />
+  <div id="vk_send_message"></div>
 </div>
 </template>
 
